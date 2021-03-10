4dab40fe409c0b63d3bf1b7c6d3d711c0073553f
forgetsemicolon <pagarwal2610@gmail.com>
2021-03-09 17:19:44 -0800

    snap scroll added

--- FinalProject/Templates/es6-webpack-template/node/src/index.js
+++ FinalProject/Templates/es6-webpack-template/node/src/index.js
@@ -6,6 +6,8 @@ import {drawStreamGraph} from "./js/streamgraph"
 import {drawBubbleChart} from "./js/bubblechart"
 import { drawGeographicMap } from './js/geographic';
 
+import scrollSnapPolyfill from 'css-scroll-snap-polyfill'
+
 /* 
     TODO: all the other logic for implementing your charts + adding in some basic filters 
     (e.g. dropdown menus for seeing different aspects of the data)
@@ -13,3 +15,9 @@ import { drawGeographicMap } from './js/geographic';
 
 drawStreamGraph();
 drawBubbleChart();
+
+window.onload = function(){
+    // wait for DOM to load
+    // let items = document.querySelectorAll('slide');
+    scrollSnapPolyfill();
+    }
